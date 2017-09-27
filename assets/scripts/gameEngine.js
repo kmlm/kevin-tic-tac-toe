11b85573d5d0855d4444b9f15c5311f907a07c8e
kmlm <kevin.mcaleese@libertymutual.com>
2017-09-27 15:08:47 -0400

    Initial isX idea

--- assets/scripts/gameEngine.js
+++ assets/scripts/gameEngine.js
@@ -1,40 +1,69 @@
-//
-// const game = []
-//
-// const createx = function (event) {
-//   event.preventDefault()
-//   $(this).text('X')
-//   game.push('x')
-//   console.log (game)
+// board array that pushing x and o to
+const game = []
+
+// checking for empty board
+
+// const board = {
+//   '#box1': ,
+//   '#box2': ,
+//   '#box3': ,
+//   '#box4': ,
+//   '#box5': ,
+//   '#box6': ,
+//   '#box7': ,
+//   '#box8': ,
+//   '#box9': ,
 // }
-//
+
+// const checkBoard = function () {
+//   for (let i =0 ; i<board.length i++) {
+//     if (board.)
+//   }
 // }
+
+
+// pushing x and o
+const createx = function (event) {
+  event.preventDefault()
+  $(this).text('X')
+  game.push('x')
+  console.log (game)
+  console.log(isX)
+}
+
+// when to use x --- place x on even or 0 and o on odd
+const isX = function (array) {
+  if (array.length === 0 || array.length % 2 === 0)
+    {return true}
+}
 // const createo = function (event) {
 //   event.preventDefault()
 //   $(this).text('O')
 //   game.push('o')
 //   console.log (game)
 // }
-// //
-// const placeLetter = function (clickx) {
-//   if (clickx === true) {
-//     createx()
-//   }
-//   else {createo()}
-// }
 
-const clickx = function () {
-  if (this  === 'x') {
-    return true
-  } else {
-    return false
-  }
+// trying to combine x and o function
+
+const placeLetter = function (isX, createx, createo) {
+  if (isX(game) === true) {
+    createx()}
+  else {createo()}
 }
 
+// const clickx = function () {
+//   if (this  === 'x') {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
 module.exports = {
-// createx,
+createx,
 // createo,
 // placeLetter,
-// game,
-clickx
+game,
+// clickx,
+isX
 }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,26 +7,28 @@ const gameEngine = require('./gameEngine')
 $(() => {
   setAPIOrigin(location, config)
 })
+
+
+// $(() => {
+//   $("#xInBox").on('click', gameEngine.clickx)
+//   $("#oInBbox").on('click', gameEngine.clickx)
+// })
+
+
+
 // Changing box to X or O
 
 $(() => {
-  $("#xInBox").on('click', gameEngine.clickx)
-  $("#oInBbox").on('click', gameEngine.clickx)
+  $("#box1").on('click', gameEngine.createx)
+  $("#box2").on('click', gameEngine.createx)
+  $("#box3").on('click', gameEngine.createx)
+  $("#box4").on('click', gameEngine.createx)
+  $("#box5").on('click', gameEngine.createx)
+  $("#box6").on('click', gameEngine.createx)
+  $("#box7").on('click', gameEngine.createx)
+  $("#box8").on('click', gameEngine.createx)
+  $("#box9").on('click', gameEngine.createx)
 })
-
-
-//
-// $(() => {
-//   $("#box1").on('click', gameEngine.createx)
-//   $("#box2").on('click', gameEngine.createx)
-//   $("#box3").on('click', gameEngine.createx)
-//   $("#box4").on('click', gameEngine.createx)
-//   $("#box5").on('click', gameEngine.createx)
-//   $("#box6").on('click', gameEngine.createx)
-//   $("#box7").on('click', gameEngine.createx)
-//   $("#box8").on('click', gameEngine.createx)
-//   $("#box9").on('click', gameEngine.createx)
-// })
 
 
 // use require with a reference to bundle the file and use it in this file
