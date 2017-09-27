838aa7e08b701ac4a08ee6c7123627d0d3864e8a
kmlm <kevin.mcaleese@libertymutual.com>
2017-09-27 13:13:53 -0400

    Using "this" to allow for every box to change to x

--- assets/scripts/gameEngine.js
+++ assets/scripts/gameEngine.js
@@ -1,4 +1,30 @@
-$('#box1').on('click', function (event) {
+//
+// const boxes = ['#box1','#box2','#box3','#box4','#box5',
+//                 '#box5','#box6','#box7','#box8','#box9']
+
+// const createx = function (event, this) {
+//   event.preventDefault()
+//   $(boxes[this]).text('X')
+// }
+
+//
+
+const createx = function (event) {
   event.preventDefault()
-  $('#box1').text('X')
-})
+  $(this).text('X')
+}
+
+const createo = function (event) {
+  event.preventDefault()
+  $(this).text('O')
+}
+
+// const placeLetter = function ()
+
+
+module.exports = {
+createx,
+createo,
+// placeLetter,
+boxes
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,10 +7,21 @@ const gameEngine = require('./gameEngine')
 $(() => {
   setAPIOrigin(location, config)
 })
+// Changing box to X or O
 
-// $(() => {
-//   $(".board").on('click',)
-// })
+$(() => {
+  $("#box1").on('click', gameEngine.createx)
+  $("#box2").on('click', gameEngine.createx)
+  $("#box3").on('click', gameEngine.createx)
+  $("#box4").on('click', gameEngine.createx)
+  $("#box5").on('click', gameEngine.createx)
+  $("#box6").on('click', gameEngine.createx)
+  $("#box7").on('click', gameEngine.createx)
+  $("#box8").on('click', gameEngine.createx)
+  $("#box9").on('click', gameEngine.createx)
+})
+
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
