86245885f30af9e58f4dfb163d195ff5dba02f2e
kmlm <kevin.mcaleese@libertymutual.com>
2017-09-28 14:05:23 -0400

    NumTurn to fix placeLetter - toString to fix win
    created numTurn and set to 0 initially each click adds a number and
    x or o alternate based on odd or even
    
    added .toString to each comparsion in in checkForWin and now it is
    working
    
    will now add check for win into placeLetter

--- assets/scripts/gameEngine.js
+++ assets/scripts/gameEngine.js
@@ -14,8 +14,14 @@ const createo = function(index) {
 
 // when to use x or o --- place x on even or 0 and o on odd
 
-const xOrO = function () {
+let numTurns = 0
 
+const whoseTurn = function() {
+  if (numTurns === 0 || numTurns % 2 === 0) {
+    return 'x'
+  } else {
+    return 'o'
+  }
 }
 // const isX = function(array) {
 //   if (array.length === 0 || array.length % 2 === 0) {
@@ -23,15 +29,15 @@ const xOrO = function () {
 //   }
 // }
 
-const isX = function () {
-    let numX = 0;
-    for (let i = 0; i < game.length; i++) {
-        if (game[i] === 'x') {
-            numX++
-        }
-    }
-    return numX;
-}
+// const isX = function () {
+//     let numX = 0;
+//     for (let i = 0; i < game.length; i++) {
+//         if (game[i] === 'x') {
+//             numX++
+//         }
+//     }
+//     return numX;
+// }
 
 // const isX = function(array) {
 //   let numX = 0
@@ -61,15 +67,16 @@ const placeLetter = function(event) {
   if ($(this).text() === 'X' || $(this).text() === 'O') {
     return // only one letter per box
   }
-  if (isX() === 0 || isX() % 2 === 0) { // x on 0 and evens and o on odds
+  if (whoseTurn() === 'x') { // x on 0 and evens and o on odds
     createx($(this).data('index')) // pushes x to array
     $(this).text('X') // changing board letter to x
   } else {
     createo($(this).data('index'))
     $(this).text('O')
   }
+  numTurns++
   console.log(game)
-  console.log(isX)
+  console.log(numTurns)
 }
 //
 
@@ -77,7 +84,7 @@ const placeLetter = function(event) {
 //
 // checking to see who won
 
-const testGame = ['x', 'x', 'x', '']
+// const testGame = ['x', 'x', 'x', '']
 // win scenarios
 
 const xWins = {
@@ -139,14 +146,14 @@ const getOIndexes = function() {
 }
 
 const checkForWin = function () {
-  if (getXIndexes() === xWins.top || getXIndexes() === xWins.middle || getXIndexes() === xWins.bottom ||
-    getXIndexes() === xWins.diagonal1 || getXIndexes() === xWins.vertLeft || getXIndexes() === xWins.vertCenter ||
-    getXIndexes() === xWins.vertRight || getXIndexes() === xWins.diagonal2) {
+  if (getXIndexes().toString() === xWins.top.toString() || getXIndexes().toString() === xWins.middle.toString() || getXIndexes().toString() === xWins.bottom.toString() ||
+    getXIndexes().toString() === xWins.diagonal1.toString() || getXIndexes().toString() === xWins.vertLeft.toString() || getXIndexes().toString() === xWins.vertCenter.toString() ||
+    getXIndexes().toString() === xWins.vertRight.toString() || getXIndexes().toString() === xWins.diagonal2.toString()) {
     console.log('x wins')
     return
-  } else if (getOIndexes() === oWins.top || getOIndexes() === oWins.middle || getOIndexes() === oWins.bottom ||
-    getOIndexes() === oWins.diagonal1 || getOIndexes() === oWins.vertLeft || getOIndexes() === oWins.vertCenter ||
-    getOIndexes() === oWins.vertRight || getOIndexes() === oWins.diagonal2) {
+  } else if (getOIndexes().toString() === oWins.top.toString() || getOIndexes().toString() === oWins.middle.toString() || getOIndexes().toString() === oWins.bottom.toString() ||
+    getOIndexes().toString() === oWins.diagonal1.toString() || getOIndexes().toString() === oWins.vertLeft.toString() || getOIndexes().toString() === oWins.vertCenter.toString() ||
+    getOIndexes().toString() === oWins.vertRight.toString() || getOIndexes().toString() === oWins.diagonal2.toString()) {
     console.log('o wins')
     return
   }
@@ -171,10 +178,14 @@ const clearGameArray = function(){
   game = ['','','','','','','','','']
 }
 
+const clearNumTurn = function() {
+  numTurn = 0
+}
 const newGame = function(event) {
   event.preventDefault()
   clearGameArray()
   clearBoard()
+  clearNumTurn()
   console.log(game)
 }
 
@@ -186,7 +197,7 @@ module.exports = {
   createo,
   placeLetter,
   game,
-  isX,
+  // isX,
   newGame,
   clearBoard,
   clearGameArray,
