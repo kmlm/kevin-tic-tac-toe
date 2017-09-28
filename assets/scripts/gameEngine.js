0f57023c47ed21549acfd8b241fb1c45fe4021be
kmlm <kevin.mcaleese@libertymutual.com>
2017-09-28 16:25:10 -0400

    Issue with gameOver running after a user signs in
    game runs when gameOver commented out

--- assets/scripts/gameEngine.js
+++ assets/scripts/gameEngine.js
@@ -1,6 +1,7 @@
+
 // board array that pushing x and o to
 let game = ['', '', '', '', '', '', '', '', '']
-let gameOver = false
+// let gameOver = false
 let numTurns = 0
 
 // pushing x and o
@@ -29,11 +30,11 @@ const whoseTurn = function() {
 const placeLetter = function(event) {
   event.preventDefault() // preventing page refresh
   $('.userMessage').text('') // initial user message that goes away
-  if (gameOver) { // whether clicks should be firing -- should not if game is over
-    console.log('did it work?')
-    $('.userMessage').text('This game is over. Click the button below to start a new game.')
-    return
-  }
+  // if (gameOver) { // whether clicks should be firing -- should not if game is over
+  //   console.log('did it work?')
+  //   $('.userMessage').text('This game is over. Click the button below to start a new game.')
+  //   return
+  // }
   if ($(this).text() === 'X' || $(this).text() === 'O') {
     return // only one letter per box
   }
@@ -46,9 +47,6 @@ const placeLetter = function(event) {
   }
   numTurns++
   checkForWin()
-  // if ($('.userMessage').text() === 'X WINS!' || $('.userMessage').text() === 'O WINS!') {
-  //
-  // }
   console.log(game)
   console.log(numTurns)
   console.log(gameOver)
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,24 +9,6 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
-// $(() => {
-//   $("#xInBox").on('click', gameEngine.clickx)
-//   $("#oInBbox").on('click', gameEngine.clickx)
-// })
-
-// Changing box to X or O
-
-// $(() => {
-//   $("#box1").on('click', gameEngine.placeLetter(0))
-//   $("#box2").on('click', gameEngine.placeLetter(1))
-//   $("#box3").on('click', gameEngine.placeLetter(2))
-//   $("#box4").on('click', gameEngine.placeLetter(3))
-//   $("#box5").on('click', gameEngine.placeLetter(4))
-//   $("#box6").on('click', gameEngine.placeLetter(5))
-//   $("#box7").on('click', gameEngine.placeLetter(6))
-//   $("#box8").on('click', gameEngine.placeLetter(7))
-//   $("#box9").on('click', gameEngine.placeLetter(8))
-// })
 
 $(() => {
   $("#box1").on('click', gameEngine.placeLetter)
